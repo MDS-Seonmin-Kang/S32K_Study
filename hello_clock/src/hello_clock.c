2b76fac26a7eb2fd2b44a766c00d7a6b22309838
MDS-Seonmin-Kang <seonmin.kang@mdsit.co.kr>
2026-02-06 09:06:42 +0900

    2. Hello Clocks
    Project Setting

---
+++ hello_clock/src/hello_clock.c
@@ -0,0 +1,25 @@
+#include "S32K144.h"            /* include peripheral declarations S32K144 */
+#include "clocks_and_modes.h"
+int lpit0_ch0_flag_counter = 0; /* LPIT0 timeout counter */
+void PORT_init (void) {
+  IP_PCC-> PCCn[PCC_PORTD_INDEX] = PCC_PCCn_CGC_MASK; /* Enable clock for PORT D */
+  IP_PTD->PDDR |= 1<<0;            /* Port D0:  Data Direction= output */
+  IP_PORTD->PCR[0] =  0x00000100;  /* Port D0:  MUX = ALT1, GPIO (to blue LED on EVB) */
+}
+void LPIT0_init (void) {
+  IP_PCC->PCCn[PCC_LPIT_INDEX] = PCC_PCCn_PCS(6);    /* Clock Src = 6 (SPLL2_DIV2_CLK)*/
+  IP_PCC->PCCn[PCC_LPIT_INDEX] |= PCC_PCCn_CGC_MASK; /* Enable clk to LPIT0 regs */
+  LPIT0->MCR = 0x00000001;    /* DBG_EN-0: Timer chans stop in Debug mode */
+                              /* DOZE_EN=0: Timer chans are stopped in DOZE mode */
+                              /* SW_RST=0: SW reset does not reset timer chans, regs */
+                           /* M_CEN=1: enable module clk (allows writing other LPIT0 regs)*/
+  LPIT0->TMR[0].TVAL = 40000000;    /* Chan 0 Timeout period: 40M clocks */
+  LPIT0->TMR[0].TCTRL = 0x00000001; /* T_EN=1: Timer channel is enabled */
+                              /* CHAIN=0: channel chaining is disabled */
+                              /* MODE=0: 32 periodic counter mode */
+                              /* TSOT=0: Timer decrements immediately based on restart */
+                              /* TSOI=0: Timer does not stop after timeout */
+                              /* TROT=0 Timer will not reload on trigger */
+                              /* TRG_SRC=0: External trigger source */
+                              /* TRG_SEL=0: Timer chan 0 trigger source is selected*/
+}
