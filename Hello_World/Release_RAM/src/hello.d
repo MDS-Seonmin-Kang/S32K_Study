b60a5dc1128fc1b8cf57c4f4176f0fc713ff16f5
Termuni <sungad9384@gmail.com>
2026-05-11 20:03:13 +0900

    #EX. REPO Clear

--- Hello_World/Release_RAM/src/hello.d
+++
@@ -1,3 +0,0 @@
-src/hello.o: ../src/hello.c \
- C\:/99_Study/WorkSpace/S32K_Study/Hello_World/include/S32K144.h
-C\:/99_Study/WorkSpace/S32K_Study/Hello_World/include/S32K144.h:
--- hello_clock/include/clocks_and_modes.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLOCKS_AND_MODES_H
-#define CLOCKS_AND_MODES_H
-
-extern void SOSC_init_8MHz(void);
-extern void SPLL_init_160MHz(void);
-extern void NormalRUNmode_80MHz(void);
-
-#endif
